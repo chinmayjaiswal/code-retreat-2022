62064174e2f0b0b726792cd6869f3ecd2fcceee9
Chinmay Jaiswal <chinmayjaiswalcj@gmail.com>
2022-11-05 11:34:16 +0530

    setup for Interation 2 | Chinmay

---
+++ app/src/main/java/code/retreat/GameOfLIfe.java
@@ -0,0 +1,7 @@
+package code.retreat;
+
+public class GameOfLIfe {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
---
+++ app/src/test/java/code/retreat/GameOfLIfeTest.java
@@ -0,0 +1,12 @@
+package code.retreat;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameOfLIfeTest {
+    @Test
+    void shouldExecute() {
+        assertTrue(1==1);
+    }
+}
